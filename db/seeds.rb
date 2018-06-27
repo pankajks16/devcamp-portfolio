8435ba8421dbf10fec5196c79a8a5d8014a7788e
Pankaj Kumar <pankaj.singh@estuate.com>
2018-06-27 16:20:45 +0530

    commiting seeds sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |n|
+	Blog.create!(
+		title: "My blog post #{n+1}",
+		body: "Blog body #{n+1} Lorem ipsum dolor sit amet, consectetur adipiscing 
+		elit, sed do eiusmod tempor incididunt ut labore et 
+			dolore magna aliqua. Ut enim ad minim veniam, 
+			quis nostrud exercitation ullamco laboris nisi 
+			ut aliquip ex ea commodo consequat. Duis aute 
+			irure dolor in reprehenderit in voluptate velit 
+			esse cillum dolore eu fugiat nulla pariatur. 
+			Excepteur sint occaecat cupidatat non proident, 
+			sunt in culpa qui officia deserunt mollit anim 
+			id est laborum."
+	)
+end
+
+puts "10 times Blog created"
+
+
+5.times do |x|
+	Skill.create!(
+		title: "Rails #{x+1}",
+		percent_utilized: 5*(x+1)
+	)
+end
+
+puts "5 times Skill created"
+
+
+9.times do |x|
+	Portfolio.create!(
+		title: "Portfolio title #{x+1}",
+		subtitle: "Portfolio service #{x+1}",
+		body: "Portfolio body #{x+1} Duis aute 
+			irure dolor in reprehenderit in voluptate velit 
+			esse cillum dolore eu fugiat nulla pariatur. 
+			Excepteur sint occaecat cupidatat",
+		main_image: "http://via.placeholder.com/600x400",
+		thumb_image: "http://via.placeholder.com/350x200"
+	)
+end
+
+
+puts "9 times Portfolio created"
