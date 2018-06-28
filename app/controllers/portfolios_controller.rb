70e7755b99d1ad57c0b580d55d54b7f38e4ebd45
Pankaj Kumar <pankaj.singh@estuate.com>
2018-06-28 16:02:13 +0530

    integrated new and create action for Portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,23 @@
 class PortfoliosController < ApplicationController
 	def index
-		@portfolios_items = Portfolio.all
+		@portfolios_items = Portfolio.all.order("created_at DESC")
+		#@portfolios_items = Portfolio.all
 	end
+
+	def new
+		@portfolios_item = Portfolio.new
+	end
+
+	def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+	end
+
 end
