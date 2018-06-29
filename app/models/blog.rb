44ba8fbfb16e552937066d83e24726d6c5320454
Pankaj Kumar <pankaj.singh@estuate.com>
2018-06-29 17:39:36 +0530

    Installed annotate and awesome_print gem via Gemfile and ran annotate command

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Blog < ApplicationRecord
 end
