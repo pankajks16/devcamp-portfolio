3e179c17c06efd1e1815cb90fe2ca6fecca16720
Pankaj Kumar <pankaj.singh@estuate.com>
2018-07-02 11:01:33 +0530

    Customized portfolio routes with our own

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :portfolios
+  resources :portfolios, except: [:show]  # except option excludes the generation of 
+  										  # natural routes for the passed value in array.
   resources :blogs
+
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'	# we overriden the general route for show action 
+
   # get 'pages/home'
 
   # get 'pages/contact'
