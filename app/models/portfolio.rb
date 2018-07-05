03c3d36f42ce14d1b512dda1566d130ec220ea7e
Pankaj Kumar <pankaj.singh@estuate.com>
2018-07-05 17:37:23 +0530

    Implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -13,6 +13,7 @@
 #
 
 class Portfolio < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
 	scope :ruby_on_rails_items, -> { where(subtitle: "Ruby On Rails") }
@@ -24,8 +25,11 @@ class Portfolio < ApplicationRecord
 	end
 
 	def set_defaults
-		self.main_image ||= "http://via.placeholder.com/600x400"
-		self.thumb_image ||= "http://via.placeholder.com/350x200"
+		self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
+		self.thumb_image ||= Placeholder.image_generator(height: 350, width: 250)
+		
+		# self.main_image ||= "http://via.placeholder.com/600x400"
+		# self.thumb_image ||= "http://via.placeholder.com/350x200"
 
 		# The above code is equivalent to:
 		
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -7,8 +7,17 @@
 #  percent_utilized :integer
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
+#  badge            :text
 #
 
 class Skill < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title, :percent_utilized
+
+	after_initialize :set_defaults
+	
+	def set_defaults
+		# self.badge ||= "http://via.placeholder.com/250x250"
+		self.badge ||= Placeholder.image_generator(height: 250, width: 250)
+	end
 end
