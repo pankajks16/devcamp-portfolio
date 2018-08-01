4fd38f439c3ff27c31e80f5522350603baaf0f03
PANKAJ KUMAR SINGH <pankajks.ece@gmail.com>
2018-08-01 17:51:14 +0530

    The Nested attribute is implemented in Portfolios app

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,11 +16,13 @@ class PortfoliosController < ApplicationController
 	end
 
 	def new
-		@portfolios_item = Portfolio.new	
+		@portfolios_item = Portfolio.new
+		3.times { @portfolios_item.technologies.build }
 	end
 
 	def create
-    	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, 
+    		technologies_attributes: [:name]))
     
 	    respond_to do |format|
 	      if @portfolio_item.save
--- app/models/concerns/placeholder.rb
+++ app/models/concerns/placeholder.rb
@@ -2,6 +2,6 @@ module Placeholder
 	extend ActiveSupport::Concern
 
 	def self.image_generator(height:, width:)				# colon is used to make a strict call to the parameter
-		"http://via.placeholder.com/#{height}x#{width}"		# Using : we need specify the parameter name as well.
+		"http://via.placeholder.com/#{height}x#{width}"		# Using : we need to specify the parameter name as well.
 	end
 end
