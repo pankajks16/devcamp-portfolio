f25edcff6d2c86fd6dbf4129e6bf7cdce0e79f6b
PANKAJ KUMAR SINGH <pankajks.ece@gmail.com>
2018-11-28 18:03:03 +0530

    Added the server name in the response header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,13 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
-  before_action :set_copyright
+  before_action :set_copyright, :server_name
 
   def set_copyright
     @copyright = PkFootTool::Renderer.copyright("Pankaj Kumar Singh", "All Rights Reserved")
   end
 
+  def server_name
+  	response.headers['Server'] = 'Puma 3.11.4'
+  end
 end
