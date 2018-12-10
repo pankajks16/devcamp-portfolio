129ed2e91e2e67a4cac2247a272a6fb44694696e
PANKAJ KUMAR SINGH <pankajks.ece@gmail.com>
2018-12-10 18:05:16 +0530

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,4 +23,54 @@ module ApplicationHelper
 	def copyright_generator
 		PkFootTool::Renderer.copyright("Pankaj Kumar Singh", "All Rights Reserved")
 	end
+
+	def nav_items
+		[
+			{
+				url: root_path,
+				title: 'Home'
+			},
+			{
+				url: about_me_path,
+				title: 'About'
+			},
+			{
+				url: contact_path,
+				title: 'Contact'
+			},
+			{
+				url: blogs_path,
+				title: 'Blog'
+			},
+			{
+				url: portfolios_path,
+				title: 'Portfolio'
+			}
+		]
+	end
+
+# 	def nav_helper style, tag_type
+# nav_links = <<NAV
+# <#{tag_type}><a href="#{root_path}" class="#{style} #{active?(root_path)}">Home</a></#{tag_type}>
+# <#{tag_type}><a href="#{about_me_path}" class="#{style} #{active?(about_me_path)}">About</a></#{tag_type}>
+# <#{tag_type}><a href="#{contact_path}" class="#{style} #{active?(contact_path)}">Contact</a></#{tag_type}>
+# <#{tag_type}><a href="#{blogs_path}" class="#{style} #{active?(blogs_path)}">Blog</a></#{tag_type}>
+# <#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active?(portfolios_path)}">Portfolio</a></#{tag_type}>
+# NAV
+# 	nav_links.html_safe
+# 	end
+
+	def nav_helper style, tag_type
+		nav_links = ''
+
+		nav_items.each do |item|
+			nav_links << "<#{tag_type}><a href='#{item[:url]}'' class='#{style} #{active?(item[:url])}'>#{item[:title]}</a></#{tag_type}>"
+		end
+		nav_links.html_safe
+	end
+
+
+	def active?(url)
+		"active" if current_page?(url)
+	end
 end
