a51b0a55f82f6cf02b9776cc984cf61b37d66f57
PANKAJ KUMAR SINGH <pankajks.ece@gmail.com>
2018-12-14 16:39:31 +0530

    Integrated video background along with the styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -64,7 +64,7 @@ module ApplicationHelper
 		nav_links = ''
 
 		nav_items.each do |item|
-			nav_links << "<#{tag_type}><a href='#{item[:url]}'' class='#{style} #{active?(item[:url])}'>#{item[:title]}</a></#{tag_type}>"
+			nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active?(item[:url])}'>#{item[:title]}</a></#{tag_type}>"
 		end
 		nav_links.html_safe
 	end
